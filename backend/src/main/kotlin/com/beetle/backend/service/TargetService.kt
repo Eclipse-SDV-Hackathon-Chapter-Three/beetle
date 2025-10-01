d0f700d2aa59fac9a66143d890b4e9bab98e17bd
ayowole.agbedejobi <ayowole.agbedejobi@engineeringhub.man>
2025-10-01 20:44:28 +0100

    save target

--- backend/src/main/kotlin/com/beetle/backend/service/TargetService.kt
+++ backend/src/main/kotlin/com/beetle/backend/service/TargetService.kt
@@ -4,13 +4,19 @@ import com.beetle.backend.client.TargetClient
 import com.beetle.backend.client.request.TargetRequest
 import com.beetle.backend.client.response.TargetResponse
 import com.beetle.backend.mapper.TargetMapper
+import com.beetle.backend.repository.TargetRepository
 import org.springframework.stereotype.Service
 
 @Service
-class TargetService(private val targetClient: TargetClient, private val targetMapper: TargetMapper) {
+class TargetService(
+    private val targetClient: TargetClient, private val targetMapper: TargetMapper,
+    private val targetRepository: TargetRepository
+) {
 
     fun createTarget(targetName: String, request: TargetRequest) {
         targetClient.createTarget(targetName, request)
+        val target = targetMapper.toEntity(request)
+        targetRepository.save(target)
     }
 
     fun getTarget(targetName: String): TargetResponse? {
